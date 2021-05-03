c305887c7f3959790f8cfaea237a30c347c86f3d
Smarttalker <a.galaychuk@gmail.com>
2021-05-03 20:32:46 +0000

    Refactored to BirthDays class

---
+++ src/Dates/BirthDays.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Smarttalker\Dates;
+
+
+class BirthDays
+{
+    private $birthDate;
+    private $todayDate;
+
+    public function __construct($birthDate)
+    {
+        $this->birthDate = $birthDate;
+        $this->todayDate = date("Y-m-d");
+    }
+
+    public function findTotalDays()
+    {
+        $totalSecs = strtotime($this->todayDate) - strtotime($this->birthDate);
+        return floor($totalSecs / 3600 / 24);
+    }
+
+}
